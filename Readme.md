41255ed5b9fd5a9352fb085d1090f2cd1a6895e0
Eduard Gizatullin <edwgiz@gmail.com>
2020-04-12 16:11:20 +0300

    Quarkus Async

--- Readme.md
+++ Readme.md
@@ -24,4 +24,4 @@ Implementation | Throughput, requests/sec | CPU, % | RAM, MB
 Micronaut-Netty Sync | 51710 | 57 | 682
 Micronaut-Netty Async | 49690 | 59 | 525
 Quarkus Sync | 36110 | 60 | 420
- 
+Quarkus Async | 33808 | 61 | 440 
--- quarkus/src/main/java/com/github/edwgiz/sample/microservice/http/performance/quarkus/SampleResource.java
+++ quarkus/src/main/java/com/github/edwgiz/sample/microservice/http/performance/quarkus/SampleResource.java
@@ -1,14 +1,15 @@
 package com.github.edwgiz.sample.microservice.http.performance.quarkus;
 
+import io.smallrye.mutiny.Uni;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 
-import static javax.ws.rs.core.Response.Status.CREATED;
-import static javax.ws.rs.core.Response.status;
+import static io.smallrye.mutiny.Uni.createFrom;
+
 
 @Path("/")
 @Consumes(MediaType.TEXT_PLAIN)
@@ -19,7 +20,7 @@ public class SampleResource {
     @Path("/sample")
     @Consumes(MediaType.TEXT_PLAIN)
     @Produces(MediaType.TEXT_PLAIN)
-    public Response sample(String value) {
-        return status(CREATED).entity("Hello " + value + "!").build();
+    public Uni<String> sample(String value) {
+        return createFrom().item(value).onItem().apply(v -> "Hello " + value + "!");
     }
 }
